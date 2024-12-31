08580566c4d14529ff900b59966548f44bcc2d9b
okadabasso <okadabasso@gmail.com>
2025-01-01 06:51:17 +0900

    id 指定で１件取得するメソッドを追加する

--- application/backend/src/admin/books/books.controller.ts
+++ application/backend/src/admin/books/books.controller.ts
@@ -1,24 +1,27 @@
 import { AppDataSource } from '@/data-source';
 import { Book } from '@/entities/Book';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException } from '@nestjs/common';
 import { generate } from 'rxjs';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('admin/books')
 export class BooksController {
-    @Get("find-all")
+    constructor(
+        @InjectRepository(Book)
+        private readonly repos: Repository<Book>,
+
+    ) {}
+    @Get()
     async findAll(): Promise<Book[]> {
-        const repos = AppDataSource.getRepository(Book);
-        return await repos.find({});
-        // const books = [
-        //     {
-        //         id: 1,
-        //         title: "Book 1",
-        //         author: "Author 1",
-        //         publishedYear: 2021,
-        //         description: "Description 1",
-        //         genre: "Genre 1"
-        //     }
-        // ];
-        // return books;
+        return await this.repos.find({});
+    }
+    @Get("find")
+    async find(id:number): Promise<Book> {
+        const book = await this.repos.findOneBy({ id });
+        if (!book) {
+            throw new NotFoundException(`Book with id ${id} not found`);
+        }
+        return book;
     }
 }
--- application/backend/src/admin/books/books.module.ts
+++ application/backend/src/admin/books/books.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from '@/entities/Book';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BooksController]
 })
 export class BooksModule {}
