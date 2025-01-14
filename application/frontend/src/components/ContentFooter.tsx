f99526ab89ebde6baa1285dcb60a7b1a982ac22c
okadabasso <okadabasso@gmail.com>
2025-01-14 22:51:00 +0000

    content footer の配置を修正する
    mb-4 と line-height でできる隙間の2倍を contenr footer とその上のマージンにする

--- application/frontend/src/components/ContentFooter.tsx
+++ application/frontend/src/components/ContentFooter.tsx
@@ -2,9 +2,9 @@ import React, { ReactNode } from 'react';
 
 const ContentFooter = ({ children }: { children: ReactNode }) => {
     return (
-        <div  className='sticky bottom-0 relative mt-2'>
-            <div className='h-4 bg-gradient-to-b from-white/0 to-white/100  w-full'></div>
-            <div className='h-4 bg-white'></div>
+        <div  className='sticky bottom-0 relative mt-4'>
+            <div className='h-3 bg-gradient-to-b from-white/0 to-white/100  w-full'></div>
+            <div className='h-3 bg-white'></div>
             <div className=' flex gap-2 bg-white pb-12'>
                 {children}
             </div>
