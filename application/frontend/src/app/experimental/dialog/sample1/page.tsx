99c7f3c189ac5ba2a838427ed2b585a92a270b1a
okadabasso <okadabasso@gmail.com>
2025-01-19 22:43:42 +0000

    ダイアログ内でスクロールするテーブルを実装する

---
+++ application/frontend/src/app/experimental/dialog/sample1/page.tsx
@@ -0,0 +1,76 @@
+'use client'
+import Button from '@/components/forms/Button';
+import React, { useState } from 'react';
+
+import CustomDialog from '@/components/CustomDialog';
+import ScrollView from '@/components/ScrollView';
+import ItemTable from '@/app/experimental/dialog/ItemTable';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/16/solid';
+import clsx from 'clsx';
+const ExperimentalDialogPage: React.FC = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const [isExpanded, setIsExpanded] = useState(false);
+    const handleExtent = () => {
+        console.log('extent');
+        setIsExpanded(!isExpanded);
+    }
+
+
+    return (
+        <div className='overflow-hidden flex flex-col' style={{ height: 'calc(100vh - 5.5rem)' }}>
+        <div className='h-6'>head</div>
+        <div id="#foo" className='flex-1 h-full flex flex-col'  style={{ height: 'calc(100vh - 7.5rem)'     }}>
+            <div  onClick={handleExtent}>
+                <div className='flex justify-between items-center py-2'>
+                    <div>検索条件</div>
+                    <Button className='' variant='text-button'  onClick={()=> handleExtent()}>{isExpanded ?( <ChevronUpIcon className='w-4 h-4' />) : (<ChevronDownIcon className='w-4 h-4' />)}</Button>
+                </div>
+                <div className={clsx('transition-height duration-300 ease-in-out', isExpanded ? 'h-12' : 'h-0')}>
+
+                </div>
+            </div>
+            <div className='scrollable-container border border-gray-400'>
+                <ItemTable />
+            </div>
+            <div className='mt-4'><Button className='pt-0.5 px-1 w-24 text-sm' onClick={()=> handleClickOpen()}>find</Button></div>
+            <div className='my-4'>
+                <div className=''><Button className='w-32'>保存</Button></div>
+
+            </div>
+        </div>
+        <CustomDialog
+                isOpen={open}
+                onClose={handleClose}
+                headerContent="Dialog Title"
+                footerContent={
+                    <Button onClick={handleClose} className='w-48'>Close</Button>
+                }
+                className=' h-[95%]'
+            >
+                <div>dialog head</div>
+                <div id="#foo2" className="flex-1 flex flex-col"
+                    style={
+                        { "height": "calc(100vh - 50rem)" }
+                    } >
+                    <div className="scrollable-container border border-gray-400">
+                        <ItemTable />
+                    </div>
+
+                </div>
+                <div>dialog footer</div>
+            </CustomDialog>    
+        </div>
+
+
+    );
+};
+
+export default ExperimentalDialogPage;
