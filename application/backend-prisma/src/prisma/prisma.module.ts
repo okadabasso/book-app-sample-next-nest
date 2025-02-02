d94a30a27ad62a30128ac7cbf5be06831e545c77
okadabasso <okadabasso@gmail.com>
2025-02-02 11:08:02 +0000

    prisma のログ出力を winston に

--- application/backend-prisma/src/prisma/prisma.module.ts
+++ application/backend-prisma/src/prisma/prisma.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { LoggingService } from '@/logging/logging.service';
+import { LoggingModule } from '@/logging/logging.module';
 
 @Global()
 @Module({
-  providers: [PrismaService],
+  providers: [PrismaService, LoggingService],
   exports: [PrismaService],
 })
 export class PrismaModule {}
--- application/backend-prisma/src/prisma/prisma.service.ts
+++ application/backend-prisma/src/prisma/prisma.service.ts
@@ -1,8 +1,38 @@
+import { LoggingService } from '@/logging/logging.service';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, 'info'| 'warn' | 'query' | 'error'> implements OnModuleInit, OnModuleDestroy {
+  constructor(
+    private readonly logger: LoggingService,
+  ) {
+    
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'warn' },
+        { emit: 'event', level: 'error' },
+      ],
+    });
+    this.$on("query", (e: Prisma.QueryEvent) => {
+      this.logger.log(`Query: ${e.query} ${e.params}`);
+    });
+
+    this.$on('info', (e: Prisma.LogEvent) => {
+      this.logger.log(e.message);
+    });
+
+    this.$on('warn', (e: Prisma.LogEvent) => {
+      this.logger.warn(e.message);
+    });
+
+    this.$on('error', (e: Prisma.LogEvent) => {
+      this.logger.error(e.message, e.target);
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
