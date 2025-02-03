9010ce69e4b7a6396e0ccda16a7dc56e97685b29
okadabasso <okadabasso@gmail.com>
2025-02-03 23:12:31 +0000

    データベース設定を .env に移動する

--- application/backend/src/data-source.ts
+++ application/backend/src/data-source.ts
@@ -1,16 +1,16 @@
 import { DataSource } from 'typeorm';
 import { join } from 'path';
-
+import "dotenv/config";
 
 export const AppDataSource = new DataSource({
     type: 'postgres', // or your preferred database type
-    host: 'pg',
-    port: 5432,
-    username: 'testuser',
-    password: 'password',
-    database: 'testdb',
-    synchronize: false,
-    logging: true,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    logging: process.env.DB_LOGGING === 'true',
     logger: 'advanced-console',
     entities: [
         join(__dirname, 'entities/**/*.{ts,js}'),
