d9e1611a583799a9606a6b05b5a7091162ae9ce2
okadabasso <okadabasso@gmail.com>
2025-02-08 05:01:18 +0000

    header と footer の隙間のレイアウトを見直す

--- application/frontend/src/components/Footer.tsx
+++ application/frontend/src/components/Footer.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-300 text-gray-500 w-full relative">
+        <footer className="bg-gray-300 text-gray-500 w-full">
           {/* footerの上 1rem 分の隙間を入れる */}
+          <div className='relative'>
+            <div className="absolute -top-4 h-4 bg-white" style={{width: "calc(100% - 16px)"}}></div>
+          </div>
           <div className="absolute -top-4 h-4 bg-white" style={{width: "calc(100% - 16px)"}}></div>
 
-          <div className="container mx-auto py-1 text-sm leading-6">
+          <div className="container mx-auto text-sm leading-6">
             &copy; 2024 Books Application All rights reserved.
           </div>
         </footer>
--- application/frontend/src/components/Header.tsx
+++ application/frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import SignInLink from './SignInLink';
 
 export default function Header() {
   return (
-    <div className='top-0 w-full z-10 bg-white relative'>
+    <div className='top-0 w-full z-10 bg-white'>
       <header className="bg-gray-800 text-white shadow w-full pr-4">
         <div className="container mx-auto py-2 px-4 flex justify-between items-center relative left-[1px]">
           <h1 className="font-bold"><Link href="/">Books Application Sample</Link> </h1>
@@ -15,7 +15,9 @@ export default function Header() {
         </div>
       </header>
       {/* ヘッダーの下 1rem 分の隙間を入れる */}
-      <div className="absolute -bottom-4 h-4 bg-white" style={{width: "calc(100% - 16px)"}}></div>
+      <div className='relative'>
+        <div className="absolute -bottom-4 h-4 bg-white" style={{width: "calc(100% - 16px)"}}></div>
+      </div>
     </div>
   );
 };
