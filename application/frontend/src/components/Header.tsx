38d1439dd361af661afc8c490f0cee830d0ee752
okadabasso <okadabasso@gmail.com>
2025-02-08 23:12:19 +0000

    firefox の場合スクロールバーがコンテンツの内側に配置されるのに対応する
    .firefox\:pr-0 を追加して firefox の場合はパディングを0 にするようにする

--- application/frontend/src/components/Header.tsx
+++ application/frontend/src/components/Header.tsx
@@ -5,7 +5,7 @@ import SignInLink from './SignInLink';
 export default function Header() {
   return (
     <div className='top-0 w-full z-10 bg-white'>
-      <header className="bg-gray-800 text-white shadow w-full pr-4">
+      <header className="bg-gray-800 text-white shadow w-full pr-4 firefox:pr-0">
         <div className="container mx-auto py-2 px-4 flex justify-between items-center relative left-[1px]">
           <h1 className="font-bold"><Link href="/">Books Application Sample</Link> </h1>
           <nav className="space-x-4">
