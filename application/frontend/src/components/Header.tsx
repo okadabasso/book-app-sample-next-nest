a9dc5362cb4ed247c02508c1c48cc3b954106a94
okadabasso <okadabasso@gmail.com>
2025-02-08 04:50:33 +0000

    デザイン調整
    コンテンツがスクロールしたときにheaderとfooterの間が不自然にならないように隙間を作る

--- application/frontend/src/components/Header.tsx
+++ application/frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import SignInLink from './SignInLink';
 
 export default function Header() {
   return (
-    <div className='top-0 w-full z-10 bg-white'>
+    <div className='top-0 w-full z-10 bg-white relative'>
       <header className="bg-gray-800 text-white shadow w-full pr-4">
         <div className="container mx-auto py-2 px-4 flex justify-between items-center relative left-[1px]">
           <h1 className="font-bold"><Link href="/">Books Application Sample</Link> </h1>
@@ -14,7 +14,8 @@ export default function Header() {
           </nav>
         </div>
       </header>
-
+      {/* ヘッダーの下 1rem 分の隙間を入れる */}
+      <div className="absolute -bottom-4 h-4 bg-white" style={{width: "calc(100% - 16px)"}}></div>
     </div>
   );
 };
