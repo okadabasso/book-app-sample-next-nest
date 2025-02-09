0f160acf657fa84d480a3473244dbcfbc6101f5a
okadabasso <okadabasso@gmail.com>
2025-02-09 00:14:53 +0000

    デザイン調整
    ドロップダウンメニューの幅を調整する

--- application/frontend/src/components/SignInLink.tsx
+++ application/frontend/src/components/SignInLink.tsx
@@ -34,15 +34,15 @@ export default function SignInLink() {
                             <UserIcon className="inline h-4 w-4 mr-1" /> {user?.name}
                             <ChevronDownIcon className="inline ml-1 h-5 w-5 text-white"/></MenuButton>
                         <MenuItems anchor="bottom" 
-                            style={{ width: `${buttonWidth}px` }}  
+                            style={{ width: `${buttonWidth}px` }}
                             modal={false}
                             className="absolute right-0 px-2 py-1 origin-top-right bg-gray-900 text-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-[100] cursor-pointer">
                             <MenuItem >
-                                <a role="button" className="inline-block w-full" href="/auth/profile">Profile</a>
+                                <a role="button" className="inline-block" href="/auth/profile">Profile</a>
                             </MenuItem>
                             <div className="border-t border-gray-400 my-1"></div>
                             <MenuItem >
-                                <a role="button" onClick={() => handleSignOut()} className="inline-block w-full"><ArrowRightStartOnRectangleIcon className="inline h-5 w-5 text-white mr-1" />Sign Out</a>
+                                <a role="button" onClick={() => handleSignOut()} className="inline-block"><ArrowRightStartOnRectangleIcon className="inline h-5 w-5 text-white mr-1" />Sign Out</a>
                             </MenuItem>
                         </MenuItems>
                     </Menu>
