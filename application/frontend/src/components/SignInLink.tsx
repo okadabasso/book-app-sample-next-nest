67f3352e8abac6c1fbb1ba6c3767a7838498be77
okadabasso <okadabasso@gmail.com>
2025-02-11 00:09:20 +0000

    SignInLink のデザインを修正する
    幅を決めようとしても headless ui の方が優先されてしまうのでボタンの幅からドロップダウンの幅を設定するのを諦める

--- application/frontend/src/components/SignInLink.tsx
+++ application/frontend/src/components/SignInLink.tsx
@@ -1,25 +1,20 @@
 'use client';
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { Menu, MenuButton, MenuItem, MenuItems, MenuSeparator } from "@headlessui/react";
 import { ArrowRightEndOnRectangleIcon, ArrowRightStartOnRectangleIcon, ChevronDownIcon, UserIcon } from "@heroicons/react/16/solid";
 import { User } from "next-auth";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export default function SignInLink() {
     const { data: session } = useSession();
     const [user, setUser] = useState<User | null>(null);
-    const buttonRef = useRef<HTMLButtonElement>(null); // ボタンの参照
-    const [buttonWidth, setButtonWidth] = useState(0); // ボタンの幅
   
 
     useEffect(() => {
         if (session) {
             setUser(session.user as User);
         }
-        if (buttonRef.current) {
-            setButtonWidth(buttonRef.current.offsetWidth); // ボタンの幅を取得して状態に設定
-          }
     }, [session]);
 
     const handleSignOut = async () => {
@@ -29,18 +24,17 @@ export default function SignInLink() {
         <span >
             {session ? (
                 <div className="inline-block relative">
-                    <Menu as="div" className="inline-block relative min-w-36">
-                        <MenuButton ref={buttonRef} className=" inline-block  hover:underline">
+                    <Menu as="div" className="inline-block relative min-w-40">
+                        <MenuButton className=" inline-block  hover:underline">
                             <UserIcon className="inline h-4 w-4 mr-1" /> {user?.name}
                             <ChevronDownIcon className="inline ml-1 h-5 w-5 text-white"/></MenuButton>
-                        <MenuItems anchor="bottom" 
-                            style={{ width: `${buttonWidth}px` }}
+                        <MenuItems anchor="bottom end" 
                             modal={false}
-                            className="absolute right-0 px-2 py-1 origin-top-right bg-gray-900 text-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-[100] cursor-pointer">
+                            className="absolute right-0 px-2 py-1 origin-top-right bg-gray-900 text-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-[100] cursor-pointer w-40">
                             <MenuItem >
                                 <a role="button" className="inline-block" href="/auth/profile">Profile</a>
                             </MenuItem>
-                            <div className="border-t border-gray-400 my-1"></div>
+                            <MenuSeparator className="my-1 h-px bg-gray-500" />
                             <MenuItem >
                                 <a role="button" onClick={() => handleSignOut()} className="inline-block"><ArrowRightStartOnRectangleIcon className="inline h-5 w-5 text-white mr-1" />Sign Out</a>
                             </MenuItem>
